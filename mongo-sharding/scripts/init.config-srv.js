27e1342de7511fd70e30ffb7b7f5dddcdbe09d29
Arthur <tocorin@yandex.ru>
2024-10-22 01:20:15 +0300

    fix - 3 конфиг сервера

--- mongo-sharding/scripts/init.config-srv.js
+++ mongo-sharding/scripts/init.config-srv.js
@@ -3,7 +3,9 @@ rs.initiate(
       _id : "config_server",
          configsvr: true,
       members: [
-        { _id : 0, host : "configSrv:27017" }
+        { _id : 0, host : "configSrv0:27017" },
+        { _id : 1, host : "configSrv1:27017" },
+        { _id : 2, host : "configSrv2:27017" }
       ]
     }
 );
