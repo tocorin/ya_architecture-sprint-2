ba42eef3ce2abe20fa92e39e4b947fdd945c14c3
Arthur <tocorin@yandex.ru>
2024-10-22 23:04:03 +0300

    add - задание 4

---
+++ sharding-repl-cache/scripts/init.router.js
@@ -0,0 +1,17 @@
+sh.addShard( "shard0/shard0-0:27017");
+sh.addShard( "shard0/shard0-1:27017");
+sh.addShard( "shard0/shard0-2:27017");
+
+sh.addShard( "shard1/shard1-0:27017");
+sh.addShard( "shard1/shard1-1:27017");
+sh.addShard( "shard1/shard1-2:27017");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+db.helloDoc.countDocuments() 
+
+
+sh.status();
